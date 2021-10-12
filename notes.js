2bf0272ef99c06513643e7ce5602bc190463f44a
janelle-bouchard <91432686+janelle-bouchard@users.noreply.github.com>
2021-10-11 20:06:44 -0400

    Finally got the valid/invalid colors to work and the name field to error correctly

--- notes.js
+++ notes.js
@@ -1,26 +1,116 @@
-let form = document.getElementById("login-form")
-let emailInput = document.getElementById("email")
+// Complete example code from class:
 
-console.log(form)
-console.log(emailInput)
+// let form = document.getElementById("login-form")
+// let emailInput = document.getElementById('email-input')
+// let checkinInput = document.getElementById('check-in')
+// let checkoutInput = document.getElementById('check-out')
+// let now = moment()
 
-form.addEventListener('submit', (event) => {
-    // sometimes you'll see e instead of event  
-    // form validation functions will live here:
-    event.preventDefault()
-    // console.log('event object', event)
-    // console.log('Form submitted!')
-    validateEmail(emailInput)
-})
+// console.log(form)
+// console.log(emailInput)
+// console.log(now)
 
-function validateEmail(input) {
-    // console.log(input)
-    if (input.value.includes('@')) {
-    // do something to show the user entered the correct info
-    input.classList.add('valid')
-} else{
-    // do something to show the user entered incorrect info
-    input.classList.add('invalid')
-}
-    // console.log(input)
-}
+// form.addEventListener('submit', (event) => {
+//     // form validation fuctions will live here
+//     event.preventDefault()
+//     validateEmail(emailInput)
+//     validateCheckin(checkinInput)
+// })
+
+
+// function validateCheckin (input) {
+//     let errorEl = document.createElement('p')
+//     errorEl.id = "checkin-error"
+//     console.log(input.value)
+//     // Cannot checkin before today
+//     if (moment(input.value).isBefore(now)) {
+//         errorEl.innerText = "You must choose a date after today"
+//         document.getElementById('check-in-div').appendChild(errorEl)
+//         console.log('date must be after today')
+//         checkinInput.classList.add('invalid')
+//     } else {
+//         if (document.getElementById('checkin-error')) {
+//             document.getElementById('checkin-error').remove()
+//         }
+//         checkinInput.classList.remove('invalid')
+//         checkinInput.classList.add('valid')
+//     }
+//     // Cannot checkout in under 5 days
+//     // Check in time no earlier than 1pm
+//     // Check out time no later than 10am
+// }
+
+// function validateEmail(input) {
+//     let errorEl = document.createElement('p')
+//     errorEl.id = 'email-error'
+
+//     let regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+//     if (regEx.test(input.value)) {
+//     console.log('test passed!')
+//     console.log(errorEl)
+//         // do something to show the user entered correct info
+//         input.classList.remove('invalid')
+//         input.classList.add('valid')
+//         document.getElementById('email-error').remove()
+
+//     } else if (input.value === "") {
+//         errorEl.innerText = 'Email is required'
+//         document.getElementById('email-div').appendChild(errorEl)
+//         input.classList.remove('valid')
+//     }
+    
+//     else {
+//         // do something to show the user entered incorrect info
+//         if (document.getElementById('email-error')) {
+//             document.getElementById('email-error').remove()
+//         }
+//         errorEl.innerText = 'Must enter valid email address'
+//         document.getElementById('email-div').appendChild(errorEl)
+//         input.classList.remove('valid')
+//         input.classList.add('invalid')
+//     }
+// }
+
+// Tried to follow along in class:
+
+// let form = document.getElementById("login-form")
+// let emailInput = document.getElementById("email")
+// let checkinInput = document.getElementById("check-in")
+// let checkoutInput = document.getElementById("check-out")
+
+
+// console.log(form)
+// console.log(emailInput)
+
+// form.addEventListener('submit', (event) => {
+//     // sometimes you'll see e instead of event  
+//     // form validation functions will live here:
+//     event.preventDefault()
+//     // console.log('event object', event)
+//     // console.log('Form submitted!')
+//     validateEmail(emailInput)
+// })
+
+// function validateEmail(input) {
+//     // console.log(input)
+//     let errorEl = document.createElement('p')
+//     let regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+//         if (regEx.test(input.value)) {
+//             console.log("Test passed!")
+//     // if inputText.value.match(regExp)
+//     // if (input.value.includes('@'))
+//     // do something to show the user entered the correct info
+//             input.classList.remove('invalid')
+//             input.classList.add('valid')
+//         } else {
+//     // do something to show the user entered incorrect info
+//             errorEl.innerText = 'Must enter a valid email address'
+//             document.getElementById('email-div').appendChild(errorEl)
+//             input.classList.remove('valid')
+//             input.classList.add('invalid')
+// }
+//     // console.log(input)
+// }
+
+// function validateCheckin()
--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-console.log('Add validation!');
+// console.log('Add validation!')
+
+let form = document.getElementById("parking-form")
+let nameInput = document.getElementById('name')
+let nameLabel = document.getElementById('name-label')
+let carYearInput = document.getElementById('car-year')
+let carMakeInput = document.getElementById('car-make')
+let carModelInput = document.getElementById('car-model')
+let startDateInput = document.getElementById('start-date')
+let numDaysInput = document.getElementById('days')
+let cardNumInput = document.getElementById('credit-card')
+let cvvInput = document.getElementById('cvv')
+let expDateInput = document.getElementById('expiration')
+let submit = document.getElementById('submit-button')
+let total = document.getElementById('total')
+let errorElement = document.createElement('p')
+
+// let inputFields = document.querySelectorAll(".input-field")
+
+
+form.addEventListener ("submit", (event) => {
+    event.preventDefault()
+    validateName(nameInput)
+    // validateYear(carYearInput)
+    // validateMake(carMakeInput)
+    // validateModel(carModelInput)
+    // validateStartDate(startDateInput)
+    // validateDays(numDaysInput)
+    // validateCardNum(cardNumInput)
+    // validateCardCVV(cvvInput)
+    // validateCardExpDate(expDateInput)
+    // total.innerText = "$" + (days.value *= 5)
+})
+
+// Name field must contain text
+function validateName (input) {
+    errorElement.id = "name-error"
+    console.log(input.value)
+    if (input.value === "") {
+        errorElement.innerText = 'Name is required!'
+        document.getElementById('name-field').appendChild(errorElement)
+        nameInput.classList.remove('input-valid')
+        nameLabel.classList.remove('input-valid')
+        nameInput.classList.add('input-invalid')
+        nameLabel.classList.add('input-invalid')
+    } else { 
+        document.getElementById('name-error').remove()
+        nameInput.classList.remove('input-invalid')
+        nameLabel.classList.remove('input-invalid')
+        nameInput.classList.add('input-valid')
+        nameLabel.classList.add('input-valid')
+    } 
+}
+// validateYear(input)
+// validateMake(input)
+// validateModel(input)
+// validateStartDate(input)
+// validateDays(input)
+// validateCardNum(input)
+// validateCardCVV(input)
+// validateCardExpDate(input)
+// function 
+
